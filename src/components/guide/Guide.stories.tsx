2c0764125bdcbe7ae30fad55da2c18e2ec4fe733
Eric Choi <chackhangun@gmail.com>
2024-05-12 00:50:52 +0900

    ✨ Guide 구현완료

---
+++ src/components/guide/Guide.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Guide } from "./Guide";
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Guide",
+  component: Guide,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  // args: { onClick: fn() },
+  decorators: [
+    (Story) => (
+      <div style={{ width: "500px", height: "3s00px" }}>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Guide>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const TestGuide: Story = {
+  args: {
+    title: "테스트 가이드",
+    content: "내용이 없습니다1.\n내용이 없습니다2.\n내용이 없습니다3.",
+  },
+};
