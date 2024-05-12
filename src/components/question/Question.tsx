846b55317778d00ee986e202d0cc0706c6d33e7a
Eric Choi <chackhangun@gmail.com>
2024-05-12 10:28:15 +0900

    ✨ [컴포넌트] Question 구현

--- src/components/question/Question.tsx
+++ src/components/question/Question.tsx
@@ -1,3 +1,7 @@
-export function Question() {
-  return <div></div>;
+export function Question(props: { children: React.ReactNode }) {
+  return (
+    <div className="w-full text-2xl justify-center font-semibold text-gray-900">
+      {props.children}
+    </div>
+  );
 }
