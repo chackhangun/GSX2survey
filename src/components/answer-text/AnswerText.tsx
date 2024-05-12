a061ec1a89450f506264c460acb531bd31f1780e
Eric Choi <chackhangun@gmail.com>
2024-05-12 10:27:40 +0900

    ✨ [컴포넌트] AnswerText 구현

--- src/components/answer-text/AnswerText.tsx
+++ src/components/answer-text/AnswerText.tsx
@@ -1,3 +1,17 @@
-export function AnswerText() {
-  return <div></div>;
+export function AnswerText(props: {
+  children: React.ReactNode;
+  active: boolean;
+  onClick: () => void;
+}) {
+  const { children, active, onClick } = props;
+  return (
+    <div
+      className={`w-full box-border p-4 flex justify-center items-center border border-gray-300 rounded-[8px] text-base font-medium cursor-pointer
+    ${active ? "bg-primary-50 border-primary-500 text-primary-500" : "text-gray-500 hover:border-primary-500 hover:text-primary-500 hover:font-semibold"}
+    `}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
 }
